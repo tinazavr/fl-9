c183d1600c8288bd287657373021694f644e9e81
tinazavr <zavorotinskaya.ns@gmail.com>
2018-08-19 22:42:08 +0300

    Done second task.

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,21 +1,14 @@
-// Your code goes here
-const price = prompt("Input amount of money:");
-const discount = prompt("Input the discount:");
+const price = prompt('Input amount of money:');
+const discount = prompt('Input the discount:');
 const saved = price * discount / 100;
 const priceWithDiscount = price - saved;
 
 if(price < 0 || discount > 100 || discount < 0){
-    console.error("Invalid data");
+    console.error('Invalid data');
 } else {
-    console.log("For input data " + price + "(price) and " + discount + "(discount):\n" +
-        "\tPrice without discount: " + price + "\n" +
-        "\tDiscount: " + discount + "\n" +
-        "\tPrice with discount: " + Number(priceWithDiscount.toFixed(2)) + "\n" +
-        "\tSaved: " + Number(saved.toFixed(2)));
+    console.log('For input data ' + price + '(price) and ' + discount + '(discount):\n' +
+        '\tPrice without discount: ' + price + '\n' +
+        '\tDiscount: ' + discount + '\n' +
+        '\tPrice with discount: ' + Number(priceWithDiscount.toFixed(2)) + '\n' +
+        '\tSaved: ' + Number(saved.toFixed(2)));
 }
-
-
-// Price without discount: 4950
-// Discount: 30%
-// Price with discount: 3465
-// Saved:  1485
--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -1,1 +1,17 @@
-// Your code goes here
+const lengthA = +prompt('Input the first side of triangle:');
+const lengthB = +prompt('Input the second side of triangle:');
+const angle = +prompt('Input the angle:');
+const maxAngle = 180;
+const aHalf = 0.5;
+const angleRad = angle * Math.PI / maxAngle;
+const lengthC = Math.sqrt(lengthA * lengthA + lengthB * lengthB - 2 * lengthA * lengthB * Math.cos(angleRad));
+const triangleSquare = aHalf * lengthA * lengthB * Math.sin(angleRad);
+const trianglePerimeter = +lengthA + lengthB + lengthC;
+if (lengthA <= 0 || lengthB <= 0 || angle >= maxAngle || angle <= 0) {
+    console.error('Invalid data');
+} else {
+    console.log('For input data ' + lengthA + '(a length), ' + lengthB + '(b length), ' + angle + '(α-angle):\n' +
+        '\tc length: ' + Number(lengthC.toFixed(2)) + '\n' +
+        '\tTriangle square: ' + Number(triangleSquare.toFixed(2)) + '\n' +
+        '\tTriangle perimeter: ' + Number(trianglePerimeter.toFixed(2)));
+}
