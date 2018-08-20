613f6af35015e219f33643fc8e0243ad0d8512ba
tinazavr <zavorotinskaya.ns@gmail.com>
2018-08-20 19:46:59 +0300

    Walidation task 1.

--- FE_9_7_homework_control-flow/homework/src/js/task1.js
+++ FE_9_7_homework_control-flow/homework/src/js/task1.js
@@ -1,28 +1,20 @@
 const login = prompt('Input login:');
 
-if(!login){
+if (!login) {
     alert('Canceled');
-}
-else if (login.length < 4)
-{
+} else if (login.length < 4) {
     alert('I don’t know any users having name length less than 4 symbols')
-}
-else if (login !== 'User') {
+} else if (login !== 'User') {
     alert('I don’t know you')
-}
-else {
+} else {
     const password = prompt('Input password:');
     if (!password) {
         alert('Canceled');
-    }
-
-    else if (password !== 'SuperUser') {
+    } else if (password !== 'SuperUser') {
         alert('Wrong password')
-    }
-    else if(new Date().getHours() < 20){
+    } else if (new Date().getHours() < 20) {
         alert('Good day!');
-    }
-    else{
+    } else {
         alert('Good evening!');
     }
 }
