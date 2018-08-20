3bcc2eeacc36756294f1d4af5b0ed068a68656a8
tinazavr <zavorotinskaya.ns@gmail.com>
2018-08-20 19:40:28 +0300

    Done task 1 HW7.

---
+++ FE_9_7_homework_control-flow/homework/src/js/task1.js
@@ -0,0 +1,28 @@
+const login = prompt('Input login:');
+
+if(!login){
+    alert('Canceled');
+}
+else if (login.length < 4)
+{
+    alert('I don’t know any users having name length less than 4 symbols')
+}
+else if (login !== 'User') {
+    alert('I don’t know you')
+}
+else {
+    const password = prompt('Input password:');
+    if (!password) {
+        alert('Canceled');
+    }
+
+    else if (password !== 'SuperUser') {
+        alert('Wrong password')
+    }
+    else if(new Date().getHours() < 20){
+        alert('Good day!');
+    }
+    else{
+        alert('Good evening!');
+    }
+}
